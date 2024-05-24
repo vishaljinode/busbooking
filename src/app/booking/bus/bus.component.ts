d5c77f4b3a85bb1a15026c6b2b8fbbfb0d138074
vishalji <vishaljinode@gmail.com>
2024-05-24 17:59:56 +0530

    implement seat icon

--- src/app/booking/bus/bus.component.ts
+++ src/app/booking/bus/bus.component.ts
@@ -10,8 +10,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class BusComponent {
   selectedDate: Date = new Date();
   today: Date = new Date();
-  selectedSeat : number = 0
-  conformSeat : number = 0
+  selectedSeat : number | undefined ;
+  conformSeat : number | undefined ;
+  totalSeats : number = 24
+  bookedSeats : [] = []
+
+
 
 
   constructor( private modalService: NgbModal){
@@ -21,12 +25,27 @@ export class BusComponent {
 
 
   seatClicked(seatNumber: number,content: any) {
-    this.selectedSeat = seatNumber;     
+    this.selectedSeat = seatNumber;   
+
+    
     this.modalService.open(content, { centered: true, backdrop: 'static', keyboard: false }); 
   }
 
   bookSeat(){
-  alert(`You clicked on Seat ${this.selectedSeat}`);
+    if(this.conformSeat ==  this.selectedSeat){     
+
+      alert(`You clicked on Seat ${this.selectedSeat}`);
+      this.conformSeat = undefined
+      this.selectedSeat = undefined
+      return
+    }else{
+      this.conformSeat = undefined
+      this.selectedSeat = undefined
+      alert(`Please Select Valid Seat`);
+      return
+      
+    }
+ 
   }
 
   closemodal(){
