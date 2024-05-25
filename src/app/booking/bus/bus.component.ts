a8cf9c00948e56f971b3e0e853aa3cf39a2201a0
vishalji <vishaljinode@gmail.com>
2024-05-25 16:58:27 +0530

    before firebase

--- src/app/booking/bus/bus.component.ts
+++ src/app/booking/bus/bus.component.ts
@@ -1,56 +1,117 @@
-import {Component} from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bus',
-  standalone: false, 
+  standalone: false,
   templateUrl: './bus.component.html',
-  styleUrls: ['./bus.component.css']
+  styleUrls: ['./bus.component.css'],
 })
 export class BusComponent {
   selectedDate: Date = new Date();
   today: Date = new Date();
-  selectedSeat : number | undefined ;
-  conformSeat : number | undefined ;
-  totalSeats : number = 24
-  bookedSeats : [] = []
+  selectedSeat: number | undefined;
+  conformSeat: number | undefined;
+  totalSeats: number = 24;
+  availableSeats: number = 0;
+  bookedSeats: any = [];
 
+  name: string = '';
+  mobileNo: String | undefined;
 
+  needName: boolean = false;
 
+  constructor(private modalService: NgbModal) {}
 
-  constructor( private modalService: NgbModal){
-
+  ngOnInit(): void {
+    this.availableSeats = this.totalSeats - this.bookedSeats.length;
   }
 
+  seatClicked(seatNumber: number, content: any) {
+    this.resetModalFields();
+    this.needName = false;
 
-
-  seatClicked(seatNumber: number,content: any) {
-    this.selectedSeat = seatNumber;   
-
-    
-    this.modalService.open(content, { centered: true, backdrop: 'static', keyboard: false }); 
-  }
-
-  bookSeat(){
-    if(this.conformSeat ==  this.selectedSeat){     
-
-      alert(`You clicked on Seat ${this.selectedSeat}`);
-      this.conformSeat = undefined
-      this.selectedSeat = undefined
-      return
-    }else{
-      this.conformSeat = undefined
-      this.selectedSeat = undefined
-      alert(`Please Select Valid Seat`);
-      return
-      
+    if (this.bookedSeats.includes(seatNumber)) {
+      alert('Sorry ! This seat is already booked..');
+      return;
+    } else {
+      this.modalService.open(content, {
+        centered: true, 
+        backdrop: 'static',
+        keyboard: false,
+      });
+      this.selectedSeat = seatNumber;
     }
+  }
+
+  bookSeat() {
+    if (this.conformSeat == this.selectedSeat) {
+      alert(`You clicked on Seat ${this.selectedSeat}`);
+
+      this.bookedSeats.push(this.selectedSeat);
+      this.availableSeats = this.totalSeats - this.bookedSeats.length;
+
+      this.conformSeat = 0;
+      this.selectedSeat = 0;
+      this.resetModalFields();
+      return;
+    } else {
+      this.conformSeat = 0;
+      this.selectedSeat = 0;
+      this.resetModalFields();
+
+      alert(`Please Select Valid Seat`);
+
+      return;
+    }
+  }
+
+  closemodal() {
+    this.modalService.dismissAll();
+    this.selectedSeat = undefined;
+    this.conformSeat = undefined;
+    this.resetModalFields();
+  }
+
+  isSeatBooked(seatNumber: number): boolean {
+    return this.bookedSeats.includes(seatNumber);
+  }
+
+  nametoggle() {
+    this.needName = !this.needName;
+    this.resetModalFields();
  
   }
 
-  closemodal(){
-    this.selectedSeat =0 
-    this.modalService.dismissAll();
+  isBookingValid = () => {
+    if (
+      this.needName &&
+      this.name != '' &&
+      this.mobileNo != '' &&
+      this.mobileNo != undefined &&
+      this.mobileNo != null &&
+      this.mobileNo.length == 10 &&
+      this.selectedSeat != 0 &&
+      this.selectedSeat != undefined &&
+      this.selectedSeat != null &&
+      this.selectedSeat == this.conformSeat
+    ) {
+      return false;
+    } else if (
+      !this.needName &&
+      this.selectedSeat == this.conformSeat &&
+      this.selectedSeat != 0 &&
+      this.selectedSeat != undefined &&
+      this.selectedSeat != null
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  resetModalFields() {  
+    this.name = '';
+    this.mobileNo = '';
   }
- 
 }
