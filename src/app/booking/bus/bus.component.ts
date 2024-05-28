a1f4d7f0d11bc9e977da3d8935706ee1870dd39f
vishalji <vishaljinode@gmail.com>
2024-05-28 17:42:16 +0530

    backup

--- src/app/booking/bus/bus.component.ts
+++ src/app/booking/bus/bus.component.ts
@@ -28,6 +28,9 @@ export class BusComponent {
   }
 
   seatClicked(seatNumber: number, content: any) {
+
+    console.log("content--->",content)
+    
     this.resetModalFields();
     this.needName = false;
 
